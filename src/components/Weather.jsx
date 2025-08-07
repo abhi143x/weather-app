097e20023ba76a11b8d8abba8504b5e0edda8d60
abhi143x <iabhirajverma35@gmail.com>
2025-08-07 19:04:15 +0530

    some changes in css

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -85,31 +85,33 @@ const Weather = () => {
   }, []);
 
   return (
-    <div className="weather">
-      <div className="search-bar">
-        <input ref={inputRef} type="text" placeholder="Search" />
-        <img
-          src={search_icon}
-          alt=""
-          onClick={() => search(inputRef.current.value)}
-        />
-      </div>
-      <img src={weatherData.icon} alt="" className="weather-icon" />
-      <p className="temp">{weatherData.temp}°c</p>
-      <p className="location">{weatherData.location}</p>
-      <div className="weather-data">
-        <div className="col">
-          <img src={humidity_icon} alt="" />
-          <div>
-            <p>{weatherData.humidity} %</p>
-            <span>Humidity</span>
-          </div>
+    <div className="container">
+      <div className="weather">
+        <div className="search-bar">
+          <input ref={inputRef} type="text" placeholder="Search" />
+          <img
+            src={search_icon}
+            alt=""
+            onClick={() => search(inputRef.current.value)}
+          />
         </div>
-        <div className="col">
-          <img src={windSpeed_icon} alt="" />
-          <div>
-            <p>{weatherData.windSpeed} km/h</p>
-            <span>Wind speed</span>
+        <img src={weatherData.icon} alt="" className="weather-icon" />
+        <p className="temp">{weatherData.temp}°c</p>
+        <p className="location">{weatherData.location}</p>
+        <div className="weather-data">
+          <div className="col">
+            <img src={humidity_icon} alt="" />
+            <div>
+              <p>{weatherData.humidity} %</p>
+              <span>Humidity</span>
+            </div>
+          </div>
+          <div className="col">
+            <img src={windSpeed_icon} alt="" />
+            <div>
+              <p>{weatherData.windSpeed} km/h</p>
+              <span>Wind speed</span>
+            </div>
           </div>
         </div>
       </div>
